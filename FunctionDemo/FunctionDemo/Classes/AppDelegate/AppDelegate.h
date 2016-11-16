35f9596cb1437e292d0b8c4a0d84134bcb8e4a37
hyp <hyp@hyp.local>
2016-11-16 14:17:55 +0800

    项目部署

---
+++ FunctionDemo/FunctionDemo/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionDemo
+//
+//  Created by hyp on 2016/11/16.
+//  Copyright © 2016年 youfenghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
